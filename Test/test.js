814990fa38a265ba369d7d05428276dec737a5d9
Brandon Ortiz <bortiz@student.neumont.edu>
2021-08-18 19:32:57 -0600

    test2

--- Test/test.js
+++ Test/test.js
@@ -1,36 +1,3 @@
-// function temp() {
-//     let num1 = 2;
-//     let num2 = 8;
-//     let expected = 10;
-//     let isValid = add(num1, num2) == expected;
-//     console.log("Add ", isValid));
-// }
-
-// temp();
-/*
-const body = document.getElementsByTagName('body');
-const testId = 'b363f7e6-5bd5-4e4c-8bea-250868a2a12d';
-
-async function testCheckInput() {
-
-    let json = await checkInput(testId);
-    let isValid = json != 'QuizID not found';
-
-    body.innerHTML = `CheckInput: ${isValid}`;
-}
-
-function testParse() {
-    
-
-
-    body.innerHTML = '';
-}*/
-
-
-////// Call all functions
-
-
-
 (async() => {
 
     const testId = 'b363f7e6-5bd5-4e4c-8bea-250868a2a12d';
@@ -44,5 +11,4 @@ function testParse() {
     }
 
     testInput();
-
 });
